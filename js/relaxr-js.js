ba7e95c3b42e1103071cda7307f19a5edd299a4d
Emily Walker <emilycwalker@gmail.com>
2015-09-23 14:03:59 +1000

    slide up working

--- js/relaxr-js.js
+++ js/relaxr-js.js
@@ -8,6 +8,16 @@ $( document ).ready(function() {
 
 	$( '.readmore').click(function() {
 		$('#show-this-on-click').slideDown("slow");
+		$('.readmore').hide();
+		$('.readless').show();
+  	});
+
+
+  	$( '.readless').click(function(){
+  		$('#show-this-on-click').slideUp("slow");
+  		$('.readless').hide();
+		$('.readmore').show("slow");
+
   	});
 
 });
